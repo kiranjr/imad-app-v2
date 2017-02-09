37c60ce61c28326a1f6f911192b6d4dcfb6590d7
kiranjr <kiranbabuhanuman@gmail.com>
2017-02-09 20:57:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function (req,res) {
+    res.send('article one has requested and served here');    
+});
+
+app.get('/article-second',function (req,res) {
+    res.send('article second has requested and served here');    
+});
+
+app.get('/article-third',function (req,res) {
+    res.send('article third has requested and served here');    
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
