ce707002f569b8f9d71d84c480d4bf012a178650
kiranjr <kiranbabuhanuman@gmail.com>
2017-03-10 12:58:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool = require('pg').pool;
+var Pool = require('pg').Pool;
 
 var config = {
-  username: 'kiranjr',
+  user: 'kiranjr',
   database: 'kiranjr',
   host: 'db.imad.hasura-app.io',
   port: '5432',
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new pool(config);
+var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     pool.query('SLEECT * FROM test',function(err,result){
         if(err){
