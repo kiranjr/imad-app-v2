3bdc6f765a4350bd9048fb839c0035730d2f86ad
kiranjr <kiranbabuhanuman@gmail.com>
2017-03-10 12:16:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,20 +15,7 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
-var pool = new Pool(config);
-app.get('/test-db', function(req, res) {
-    pool.query('SELECT * FROM test', function (err, result){
-        if(err)
-        {
-            res.status(500).send(err.toString());
-        }
-        else
-        {
-            res.send(JSON.stringify(result));
-        }
-    });
-});
-
+v
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
