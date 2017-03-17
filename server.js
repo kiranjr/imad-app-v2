b05675ba7fc0cee96a7c052af84c101a5f54ce4d
kiranjr <kiranbabuhanuman@gmail.com>
2017-03-17 21:03:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,24 +11,24 @@ app.get('/', function (req, res) {
 
 var articles = {
      'article-one' : {
-                title: 'Article one | Kiranjr.',
-                heading: 'Article-one',
+                title: 'article one | Kiranjr.',
+                heading: 'article-one',
                 date: 'FEB 9,2017',
                 content: `<p>
                                 hi!this my first content in my first webapp ,hope so that i will learn it little more when i need this .
                             </p>`
             },
      'article-second' : {
-        title: 'Article second | kiranjr.',
-        heading: 'Article-second',
+        title: 'article second | kiranjr.',
+        heading: 'article-second',
         date: 'MAR 17,2017',
         content: `<p>
                     hi ! This My Second Article.
                   </p>`
     },
      'article-third' : {
-        title: 'Article third | kiranjr.',
-        heading: 'Article-third',
+        title: 'article third | kiranjr.',
+        heading: 'article-third',
         date: 'OCT 22,2017',
         content: `
             <p>
