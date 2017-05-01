93e26c6c75e8e6e17509c8e648b5509b36777a7c
kiranjr <kiranbabuhanuman@gmail.com>
2017-05-01 20:19:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,76 +30,9 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html')); 
 });
 
-var articles = {
-     'article-one' : {
-                title: 'Article one | Kiranjr.',
-                heading: 'Article-one',
-                date: 'FEB 9,2017',
-                content: `<p>
-                                hi!this my first content in my first webapp ,hope so that i will learn it little more when i need this .
-                            </p>`
-            },
-     'article-second' : {
-                title: 'Article second | kiranjr.',
-                heading: 'Article-second',
-                date: 'MAR 17,2017',
-                content: `<p>
-                            hi ! This My Second Article.
-                          </p>`
-            },
-     'article-third' : {
-                title: 'Article third | kiranjr.',
-                heading: 'Article-third',
-                date: 'OCT 22,2017',
-                content: `
-                    <p>
-                        Hi ! This My Third Article.<br>
-                        And This My New Line.
-                    </p>` 
-            }
-};
 
-function createtemplate(data){
-    var title = data.title;
-    var date = data.date;
-    var heading = data.heading;
-    var content = data.content;
-    var htmltemplate = `
-            <html>
-            <head>
-                <title>
-                    ${title}
-                </title>
-                <meta name="viewpot" content="width-device-width , initial-scale =1" />
-                <link href="/ui/style.css" rel="stylesheet" />
-            </head>
-            <body>
-                <div class="container">
-                    <div>
-                        <a href='/'>home</a>
-                    </div>
-                    <hr/>
-                    <h3>
-                        ${heading}
-                    </h3>
-                    <div>
-                        ${date}
-                    </div>
-                    <div>
-                        <p>
-                            ${content}
-                        </p>
-                    </div>
-                </div>
-            </body>
-        </html>
-    `;
-    return htmltemplate;
-}
-
-app.get('/:articlename',function (req,res) {
-    var articlename = res.params.articlename;
-    res.send(createtemplate(articles[articlename]));   
+app.get('/article-one',function (req,res) {
+    res.sendFile(path.join(__dirname, 'ui','article-one.html'));   
 });
 
 function hash (input, salt){
