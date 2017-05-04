e785855c290336db7d61a870b26ef23d08250122
kiranjr <kiranbabuhanuman@gmail.com>
2017-05-04 17:25:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html')); 
 });
 
-app.get('/article-one',function (req,res){
-   res.sendfile(path.join(__dirname,'ui','article-one.html')); 
+app.get('/',function (req,res){
+   res.sendfile(path.join(__dirname,'ui','chevy.html')); 
 });
 
 function hash (input, salt){
